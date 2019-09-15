9866f0429d085b1c2b6597ffb6dda1ad06c1ec1b
barnstorm3r <barnabyc@sky.com>
2019-09-15 18:26:45 +0100

    Anonymise IPs to conform to GDPR

--- js/main.js
+++ js/main.js
@@ -101,4 +101,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', gaProperty);
+gtag('config', gaProperty, {'anonymize_ip': true});
