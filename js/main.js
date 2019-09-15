231f6b47d483ee3e3a92f8757bbc7f49584d2830
barnstorm3r <barnabyc@sky.com>
2019-09-15 18:06:13 +0100

    Add cookie notice to comply with GDPR

--- js/main.js
+++ js/main.js
@@ -70,3 +70,35 @@ function checkBG() {
 checkBG(); // check for the first time
 window.addEventListener('resize', checkBG); // then, every time the window resizes, check again
 */
+
+// code to control user opt-in / opt-out for Google Analytics
+let gaProperty = 'UA-58923240-2',  disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+    window[disableStr] = true;
+    console.log('GA Disable Cookie detected. Opting out.');
+}
+else if (document.cookie.indexOf(disableStr + '=false') > -1) {
+    window[disableStr] = true;
+    console.log('Google Analytics Enabled. If you want to change your mind, you can either run gaOptout() in the console or delete the cookies for this domain.');
+}
+else {
+    $('#cookie-toast').toast('show');
+}
+function gaOptout() {
+    document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+    $('#cookie-toast').toast('hide');
+    return 'Succesfully opted you out of Google Analytics';
+}
+function gaOptin() {
+    document.cookie = disableStr + '=false; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    $('#cookie-toast').toast('hide');
+    return 'Succesfully opted you into Google Analytics';
+}
+
+// activate Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', gaProperty);
